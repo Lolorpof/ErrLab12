8057f2b5c546ef54b95f776335115370416b4c02
Lolorpof <sirawit_sir@cmu.ac.th>
2023-01-06 15:28:30 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int a = rand() % 9;
+    if(a == 0) cout << "You will get A in this 261102.";
+    else if(a == 1) cout << "You will get B+ in this 261102.";
+    else if(a == 2) cout << "You will get B in this 261102.";
+    else if(a == 3) cout << "You will get C+ in this 261102.";
+    else if(a == 4) cout << "You will get C in this 261102.";
+    else if(a == 5) cout << "You will get D+ in this 261102.";
+    else if(a == 6) cout << "You will get D in this 261102.";
+    else if(a == 7) cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
+
+    return 0;
+}
